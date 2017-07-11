1f1e6a4d4c7041938751610dd4e411c3e4451673
Franco Fuentes Rodrigo Arturo <rfranco@infonavit.org.mx>
2017-07-10 20:16:26 -0500

    muy cerca de un mapa funcional

--- Mapa Municipal de Jalisco.R
+++ Mapa Municipal de Jalisco.R
@@ -1,8 +1,14 @@
+install.packages("viridis")
+
 #Mapa que utiliza datos de población, señala a nivel municipal disparidad de géneros
 library(rgeos)
 library(rgdal)
 library(dplyr)
+library(foreign)
 library(ggplot2)
+library(stringr)
+library(viridis)
+
 #library(raster)
 
 
@@ -10,7 +16,6 @@ library(ggplot2)
 Catalogo_Municipal <- read.dbf("./cat_municipio_OCT2016.dbf") #mayor disparidad entre población femenina y masculina en el Estado
 Catalogo_Municipal$CVE_INEGI <- str_c(str_pad(Catalogo_Municipal$CVE_ENT, width = 2, "left", "0"), 
                                       str_pad(Catalogo_Municipal$CVE_MUN, width = 3, "left", "0"))
-
 
 #Info geográfica
 Municipios<- readOGR("./Mapa muy reducido/areas_geoestadisticas_municipales.shp",
@@ -23,21 +28,31 @@ Pais_Mapa<-fortify(Municipios, region = "id")
 Pais_Mapa$CVE_INEGI<-Pais_Mapa$id
 Pais_Mapa<-left_join(x = Pais_Mapa, y = Catalogo_Municipal, by = "CVE_INEGI")
 length(unique(Pais_Mapa$CVE_INEGI))
+Pais_Mapa$MomioMujer <-  as.numeric(Pais_Mapa$PFEM)/as.numeric(Pais_Mapa$PMAS)
 
-head(Pais_Mapa)
+
+Mapa_Municipios <- filter(.data = Pais_Mapa, CVE_ENT == "15")
+summary(Mapa_Municipios$MomioMujer)
 
 
-Muns_Map <-fortify(muns , region = "ClaveINEGI")
+
+
+theme_bare <-theme(axis.line=element_blank(),
+                   axis.text.x=element_blank(),
+                   axis.text.y=element_blank(),
+                   axis.ticks=element_blank(),
+                   axis.title.x=element_blank(),
+                   axis.title.y=element_blank(),
+                   panel.background=element_blank(),
+                   panel.border=element_blank(),
+                   panel.grid.major=element_blank(),
+                   panel.grid.minor=element_blank(),
+                   plot.background=element_blank())
+
+
+
 ggplot() +
-  geom_polygon()
-
-
-
-
-Mapa_Municipios <- filter(.data = Muns_Map, CVE_ENT == "15")
-
-ggplot() +
-  geom_polygon(data = Mapa_Municipios, aes(fill = POR_PRI_ALIANZA, 
+  geom_polygon(data = Mapa_Municipios, aes(fill = MomioMujer, 
                                            x = long,
                                            y = lat, 
                                            group = group)) +
@@ -45,15 +60,15 @@ ggplot() +
                                          y = lat, 
                                          group = group)) +
   coord_equal() +
-  theme_map() +
+  #theme_bare() +
   labs( x = NULL, 
         y = NULL, NULL,
-        title = "Votos Alianza PRI en Elección Presidencial 2012") +
+        title = "Razón entre hombres y mujeres en municipios del Estado de México") +
   theme(legend.position = "bottom") +
   scale_fill_viridis(
     option = "magma", 
     direction = -1,
-    name = "Porcentaje de Votos a favor de EPN en elección 2012",
+    name = "Proporción Mujeres/Hombres",
     guide = guide_colorbar(
       direction = "horizontal",
       barheight = unit(2, units = "mm"),
