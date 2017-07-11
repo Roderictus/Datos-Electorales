221d797ca2bedf1c580875e880340d505a765f31
Roderictus <rodrigofrancofuentes@gmail.com>
2017-07-10 21:37:55 -0500

    Mapa decente, pero del estado de méxico!

--- Mapa Municipal de Jalisco.R
+++ Mapa Municipal de Jalisco.R
@@ -1,5 +1,3 @@
-install.packages("viridis")
-
 #Mapa que utiliza datos de población, señala a nivel municipal disparidad de géneros
 library(rgeos)
 library(rgdal)
@@ -8,12 +6,16 @@ library(foreign)
 library(ggplot2)
 library(stringr)
 library(viridis)
-
 #library(raster)
 
-
-
-Catalogo_Municipal <- read.dbf("./cat_municipio_OCT2016.dbf") #mayor disparidad entre población femenina y masculina en el Estado
+Catalogo_Municipal <- read.dbf("./cat_municipio_OCT2016.dbf",as.is = TRUE) #mayor disparidad entre población femenina y masculina en el Estado
+Catalogo_Municipal$PTOT<- as.numeric(Catalogo_Municipal$PTOT)
+Catalogo_Municipal$PMAS<- as.numeric(Catalogo_Municipal$PMAS)
+Catalogo_Municipal$PFEM<- as.numeric(Catalogo_Municipal$PFEM)
+Catalogo_Municipal$VTOT<- as.numeric(Catalogo_Municipal$VTOT)
+#Dos municipios nuevos que aparecen sin población, Bacalar y Puerto Morelos, los dos son municipios nuevos
+#nas<-is.na(as.numeric(as.character(Catalogo_Municipal$PTOT)))
+#Catalogo_Municipal[nas, ]
 Catalogo_Municipal$CVE_INEGI <- str_c(str_pad(Catalogo_Municipal$CVE_ENT, width = 2, "left", "0"), 
                                       str_pad(Catalogo_Municipal$CVE_MUN, width = 3, "left", "0"))
 
@@ -29,51 +31,88 @@ Pais_Mapa$CVE_INEGI<-Pais_Mapa$id
 Pais_Mapa<-left_join(x = Pais_Mapa, y = Catalogo_Municipal, by = "CVE_INEGI")
 length(unique(Pais_Mapa$CVE_INEGI))
 Pais_Mapa$MomioMujer <-  as.numeric(Pais_Mapa$PFEM)/as.numeric(Pais_Mapa$PMAS)
-
-
 Mapa_Municipios <- filter(.data = Pais_Mapa, CVE_ENT == "15")
 summary(Mapa_Municipios$MomioMujer)
 
+####################################################################################
+######################  Elementos específicos al mapa  #############################
+####################################################################################
+theme_map <- function(...) {
+  theme_minimal() +
+    theme(
+      text = element_text(family = "sans", color = "#22211d"),
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      # panel.grid.minor = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.major = element_line(color = "#ebebe5", size = 0.2),
+      panel.grid.minor = element_blank(),
+      plot.background = element_rect(fill = "#f5f5f2", color = NA), 
+      panel.background = element_rect(fill = "#f5f5f2", color = NA), 
+      legend.background = element_rect(fill = "#f5f5f2", color = NA),
+      panel.border = element_blank(),
+      ...
+    )
+}
+####################### Clases discretas ###################################
 
-
-
-theme_bare <-theme(axis.line=element_blank(),
-                   axis.text.x=element_blank(),
-                   axis.text.y=element_blank(),
-                   axis.ticks=element_blank(),
-                   axis.title.x=element_blank(),
-                   axis.title.y=element_blank(),
-                   panel.background=element_blank(),
-                   panel.border=element_blank(),
-                   panel.grid.major=element_blank(),
-                   panel.grid.minor=element_blank(),
-                   plot.background=element_blank())
-
-
+#https://timogrossenbacher.ch/2016/12/beautiful-thematic-maps-with-ggplot2-only/
+no_classes <- 6
+labels <- c()
+quantiles <-quantile(Mapa_Municipios$MomioMujer,
+                     probs = seq(0,1,length.out = no_classes + 1 ))
+labels <- c()
+for(idx in 1:length(quantiles)) {
+  labels<-c(labels, paste0(round(quantiles[idx],2),
+                           "-",
+                           round(quantiles[idx + 1],2)))
+}
+labels <- labels[1:length(labels)-1]
+Mapa_Municipios$MomioMujer_cuantil <- cut(Mapa_Municipios$MomioMujer,
+                                          breaks = quantiles, 
+                                          labels = labels, 
+                                          include.lowest = T)
 
 ggplot() +
-  geom_polygon(data = Mapa_Municipios, aes(fill = MomioMujer, 
+  geom_polygon(data = Mapa_Municipios, aes(fill = MomioMujer_cuantil, 
                                            x = long,
                                            y = lat, 
                                            group = group)) +
   geom_path(data = Mapa_Municipios, aes( x = long, 
                                          y = lat, 
-                                         group = group)) +
+                                         group = group),
+            color = "white", size = 0.2) +
   coord_equal() +
-  #theme_bare() +
+  theme_map() +
   labs( x = NULL, 
         y = NULL, NULL,
-        title = "Razón entre hombres y mujeres en municipios del Estado de México") +
+        title = "Razón entre mujeres y hombres en municipios \n                      del Estado de México") +
   theme(legend.position = "bottom") +
   scale_fill_viridis(
-    option = "magma", 
+    option = "viridis",
+    name = "Proporción Mujeres/ Hombres",
+    discrete = T,
     direction = -1,
-    name = "Proporción Mujeres/Hombres",
-    guide = guide_colorbar(
-      direction = "horizontal",
-      barheight = unit(2, units = "mm"),
-      barwidth = unit(50, units = "mm"),
+    guide = guide_legend(
+      keyheight = unit(5, units = "mm"),
       title.position = 'top',
-      title.hjust = 0.5,
-      label.hjust = 0.5
+      reverse = T
     ))
+  
+  
+  
+  #scale_fill_viridis(
+  #  option = "viridis", 
+  #  direction = -1,
+  #  name = "Proporción Mujeres/Hombres",
+  #  guide = guide_colorbar(
+  #    direction = "horizontal",
+  #    barheight = unit(2, units = "mm"),
+  #    barwidth = unit(50, units = "mm"),
+  #    title.position = 'top',
+  #    title.hjust = 0.5,
+  #    label.hjust = 0.5
+  #  ))
